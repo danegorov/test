4f30ea0b1a170908bb9eded576007b5710e2f6c9
danegorov <egdanik171@yandex.ru>
2020-10-09 14:04:41 +0300

    update

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -5,6 +5,7 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Я работаю");
         System.out.println("Я изменился");
+        System.out.println("Я изменпился 2");
 	// write your code here
     }
 }
