31d52bbfd2d57362ba7c886d0b533061e09d91ff
danegorov <egdanik171@yandex.ru>
2020-10-09 13:38:06 +0300

    i am changed

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -4,6 +4,7 @@ public class Main {
 
     public static void main(String[] args) {
         System.out.println("Я работаю");
+        System.out.println("Я изменился");
 	// write your code here
     }
 }
